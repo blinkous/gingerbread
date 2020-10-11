5811b96b5bc81507a84b94b8ec184ad7b1d5fe5b
blinkous <saradhani1@gmail.com>
2020-10-11 16:54:59 -0400

    cleaned up comment

--- server/app.js
+++ server/app.js
@@ -1,25 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
-// const createError = require("http-errors");
-
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
@@ -30,7 +8,6 @@ const app = express();
 
 // view engine setup
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, "client/build")));
 app.use(express.static(path.join(__dirname, "./")));
 
 app.use(logger("dev"));
