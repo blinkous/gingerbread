fb5aa2dff208790f3e24f7c9d58007c010929f73
blinkous <saradhani1@gmail.com>
2020-10-13 19:56:48 -0400

    added transitional styling for title and input

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles/Home.css";
 import SearchBox from "./SearchBox";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
 
   const getData = async (searchQuery) => {
     try {
@@ -19,30 +20,23 @@ export default function Home() {
   const onSearch = (value) => {
     console.log("Received Input: ", value);
     // getData(value);
+    setShow((p) => !p);
   };
 
-  useEffect(() => {
-    // getData();
-  }, []);
-
   return (
-    <div className="App">
-      <section className="section sect-1">
-        <h1 id="main-title" className="heading">
-          Gingerbread
-        </h1>
-        <SearchBox onSearch={onSearch} />
-      </section>
-      <section className="section sect-2">
-        <h3 className="heading">Recipe of the Day</h3>
-        {data !== undefined &&
-          data.map((el, index) => (
-            <div className="recipe-card" key={index}>
-              <h5 className="heading">{el.title}</h5>
-              <img src={el.image} alt={el.title} />
-            </div>
-          ))}
-      </section>
+    <div id="home" className={`section sect-1 ${show ? "show" : ""}`}>
+      <h1 id="main-title" className="heading">
+        Gingerbread
+      </h1>
+      <SearchBox onSearch={onSearch} />
+      {/* <h3 className="heading">Recipe of the Day</h3> */}
+      {data !== undefined &&
+        data.map((el, index) => (
+          <div className="recipe-card" key={index}>
+            <h5 className="heading">{el.title}</h5>
+            <img src={el.image} alt={el.title} />
+          </div>
+        ))}
     </div>
   );
 }
