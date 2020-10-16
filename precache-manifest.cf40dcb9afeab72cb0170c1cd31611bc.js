482c2b2922936fba5f62469c4a72c6dcfc9211e7
blinkous <saradhani1@gmail.com>
2020-10-16 14:43:40 -0400

    Updates

---
+++ precache-manifest.cf40dcb9afeab72cb0170c1cd31611bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d421039c14b84181505c4d7b40446f8",
+    "url": "/gingerbread/index.html"
+  },
+  {
+    "revision": "aa37079ff63afc432f84",
+    "url": "/gingerbread/static/css/main.a9d057a9.chunk.css"
+  },
+  {
+    "revision": "6c5b489bc4fa530d89fa",
+    "url": "/gingerbread/static/js/2.b4827d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gingerbread/static/js/2.b4827d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa37079ff63afc432f84",
+    "url": "/gingerbread/static/js/main.1273f4b8.chunk.js"
+  },
+  {
+    "revision": "8ef099aa7397738941a5",
+    "url": "/gingerbread/static/js/runtime-main.ed94a5f9.js"
+  },
+  {
+    "revision": "8d97a91fe95ababe061ea1c3e948e419",
+    "url": "/gingerbread/static/media/gingerbread-cookies-0.8d97a91f.jpg"
+  },
+  {
+    "revision": "cf1c52ba4f295f63179f8ed355e7cb0e",
+    "url": "/gingerbread/static/media/gingerbread-cookies-1.cf1c52ba.jpg"
+  },
+  {
+    "revision": "056888639f2df5fc56b459353e2cf581",
+    "url": "/gingerbread/static/media/gingerbread-cookies-2.05688863.jpg"
+  },
+  {
+    "revision": "4a15f623aa7d34176f4bcfa4ce4e0817",
+    "url": "/gingerbread/static/media/gingerbread-cookies-3.4a15f623.jpg"
+  },
+  {
+    "revision": "328bcb1241b9fa592f109dafd59d9e29",
+    "url": "/gingerbread/static/media/gingerbread-cookies-4.328bcb12.jpg"
+  },
+  {
+    "revision": "98e7c12f9585012a5b8242bb7c15f2f1",
+    "url": "/gingerbread/static/media/gingerbread-cookies-5.98e7c12f.jpg"
+  }
+]);
